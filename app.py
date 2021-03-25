303586e4aa0a7555aabc4eeb8c8fe778e269699a
Diego Cáceres <caceres123dfcc@gmail.com>
2021-03-25 15:13:03 -0600

    Register html agregado

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/register")
+def register():
+    return render_template("registration.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -20,3 +20,37 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+/*
+************* register functionality begin
+*/
+
+function registerNewUser() {
+    var reg_user = document.getElementById("user_reg").value;
+    var reg_password = document.getElementById("passw_reg").value;
+    var reg_role = "client";
+
+    //alert(reg_user);
+    var userArray = [];
+
+    if (localStorage.getItem("lUserArray") !== null) {
+        userArray = JSON.parse(localStorage.getItem("lUserArray"));
+    }
+
+    var current_reg = {
+        user: reg_user,
+        password: reg_password,
+        role: reg_role
+    };
+
+    userArray.push(current_reg);
+
+    localStorage.setItem("lUserArray", JSON.stringify(userArray));
+
+    window.location.href = "http://localhost:5000/login"
+    //window.location.href = "http://heroku:5000/login";
+}
+
+/*
+************* register functionality end
+*/
