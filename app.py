d535587a04561ed91ea89d7fc01fa9dfbf1ebe02
Diego Cáceres <caceres123dfcc@gmail.com>
2021-04-09 14:37:37 -0600

    Proyecto

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/registration")
+def registration():
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
