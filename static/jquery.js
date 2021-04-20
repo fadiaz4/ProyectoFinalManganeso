442f5d97db1a879d2c87e0d2e81dd56c80c4b6e3
fadiaz4 <72111275+fadiaz4@users.noreply.github.com>
2021-04-20 01:02:39 -0600

    cambios1

--- static/jquery.js
+++ static/jquery.js
@@ -13,61 +13,56 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["correlativob"] = document.getElementById("correlativob").value;
+    formData["nombreb"] = document.getElementById("nombreb").value;
+    formData["preciob"] = document.getElementById("preciob").value;
     return formData;
 }
 
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("trajedebañoslista").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
+    cell1.innerHTML = data.correlativob;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.nombreb;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.preciob;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+    cell4.innerHTML = <a onClick="onEdit(this)" type="button">Editar</a>;
+    cell5 = newRow.insertCell(5);
+    cell5.innerHTML = <a onClick="onDelete(this)">Eliminar</a>;
 }
 
 function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("correlativob").value = "";
+    document.getElementById("nombreb").value = "";
+    document.getElementById("preciob").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("correlativob").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("nombreb").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("preciob").value = selectedRow.cells[2].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.correlativob;
+    selectedRow.cells[1].innerHTML = formData.nombreb;
+    selectedRow.cells[2].innerHTML = formData.preciob;
 }
 
 function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('Está seguro de querer elimnar este elemento?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.getElementById("trajedebañoslista").deleteRow(row.rowIndex);
         resetForm();
     }
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("nombreb").value == "") {
         isValid = false;
         document.getElementById("fullNameValidationError").classList.remove("hide");
     } else {
